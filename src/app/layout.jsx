33103176082893b99d83a56838dee06b917a5265
afhiya <faridrakapratama@gmail.com>
2024-06-10 12:14:05 +0700

    update icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,15 +7,29 @@ const gabarito = Gabarito({ subsets: ['latin'] })
 export const metadata = {
   title: 'ZuttoNime',
   description: 'Nonton anime gratis',
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/zuttonime.png',
+        href: '/zuttonime.png',
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/zuttonime.png',
+        href: '/zuttonime.png',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <body className={`${gabarito.className} bg-color-dark-1`} suppressHydrationWarning={true}>
-    <Navbar />
-      {children}
-    </body>
+      <body className={`${gabarito.className} bg-color-dark-1`} suppressHydrationWarning={true}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
